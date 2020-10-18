58a0012ca882b67586a71fee8663f9729182c5ce
KotaIkehara <ikehara.aurora95@gmail.com>
2020-10-18 16:06:31 +0900

    Bit全探索

--- ABC.py
+++ ABC.py
@@ -1,6 +1,12 @@
-import math
-from functools import reduce
+S = input()
 
-A, B = map(int, input().split())
-
-print(A*B//math.gcd(A, B))
+res = 0
+for i in range(2**(len(S)-1)):
+    sum = ""
+    for j in range(len(S)):
+        if((i >> j) & 1):
+            sum += S[j] + "+"
+        else:
+            sum += S[j]
+    res += eval(sum)
+print(res)
