cf81a448b190ed778a7a3699fb0351f25470901c
KotaIkehara <ikehara.aurora95@gmail.com>
2020-10-21 23:19:58 +0900

    Finish シミュレーション

--- ABC011C.py
+++ ABC011C.py
@@ -5,8 +5,8 @@ if(N in NG):
     print("NO")
     exit()
 for i in range(100):
-    N -= 3
-    if(N in NG):
+    N -= 3  # Nを0に近づける最善の手は3を選ぶこと
+    if(N in NG):  # 3ダメなら2を選ぶ（以下略）
         N += 1
         if(N in NG):
             N += 1
