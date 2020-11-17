dba279507b2107f4c535c20d9fc5375279a8c5fe
KotaIkehara <ikehara.aurora95@gmail.com>
2020-11-17 16:43:28 +0900

    ディレクトリを整理

--- m-solution2020B.py
+++
@@ -1,9 +0,0 @@
-a, b, c = map(int, input().split())
-K = int(input())
-
-for i in range(K):
-    if(a >= b):
-        b *= 2
-    elif(b >= c):
-        c *= 2
-print("Yes" if a < b < c else "No")
--- sumitrust2019B.py
+++
@@ -1,8 +0,0 @@
-import math
-N = int(input())
-
-for i in range(1, 50001):
-    if(math.floor(i*1.08) == N):
-        print(i)
-        exit()
-print(":(")
