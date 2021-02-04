24505cfc35fdd3fc7afecbf25cd5bd0fb2f527b7
KotaIkehara <ikehara.aurora95@gmail.com>
2021-02-04 11:25:47 +0900

    Finish ABC188

--- ABC.py
+++
@@ -1,8 +0,0 @@
-N, M = map(int, input().split())
-A = list(map(int, input().split()))
-maze = [[] for _ in range(N)]
-print(maze)
-for i in range(M):
-    x, y = map(lambda x: int(x)-1, input().split())
-    maze[x].append(y)
-print(maze)
